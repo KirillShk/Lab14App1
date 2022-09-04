fcfa68a96a6b3169e658438688614126de6c7022
Kirill Shlyakov <Kirill Shlyakov@DESKTOP-4PP6PL7>
2022-09-04 22:55:51 +0300

    Добавьте файлы проекта.

---
+++ Lab14App1/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab14App1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Product> products;
+        public MainWindow()
+        {
+            InitializeComponent();
+            products = new ObservableCollection<Product>();
+            products.Add(new Product()
+            {
+                NameProduct = "Картошка",
+                PriceProduct = 100,
+                ImageProduct = "Data/potateto.png",
+                CategoryProduct = Categorys.Food
+                
+            });
+            products.Add(new Product()
+            {
+                NameProduct = "Марковь",
+                PriceProduct = 50,
+                ImageProduct = "Data/сarrot.png",
+                CategoryProduct = Categorys.Food
+            });
+            products.Add(new Product()
+            {
+                NameProduct = "Холодильник",
+                PriceProduct = 15000,
+                ImageProduct= "Data/refigerator.png",
+                CategoryProduct = Categorys.Technic
+            });
+            products.Add(new Product()
+            {
+                NameProduct = "Телевизор",
+                PriceProduct = 7000,
+                ImageProduct = "Data/tv.png",
+                CategoryProduct = Categorys.Technic
+            });
+            lstBox.ItemsSource = products;
+        }
+    }
+}
---
+++ Lab14App1/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab14App1
+{
+    public enum Categorys
+    {
+        Food,
+        Technic
+    }
+    public class Product
+    {
+        public string NameProduct { get; set; }
+        public int PriceProduct { get; set; }
+        public string ImageProduct { get; set; }
+        public Categorys CategoryProduct { get; set; }
+
+        public override string ToString()
+        {
+            return NameProduct;
+        }
+    }
+}
